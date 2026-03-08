a67d6f4b785fa78fcfed8792619961a0f78fd20c
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-08 17:35:54 -0400

    Update main.tf

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,1 +1,21 @@
-# Placeholder for future Key Vault module
+resource "azurerm_key_vault" "kv" {
+  name                        = "kv-landing-zone-demo"
+  location                    = var.location
+  resource_group_name         = var.resource_group_name
+  tenant_id                   = var.tenant_id
+  sku_name                    = "standard"
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  access_policy {
+    tenant_id = var.tenant_id
+    object_id = var.object_id
+
+    secret_permissions = [
+      "Get",
+      "Set",
+      "Delete",
+      "List"
+    ]
+  }
+}
