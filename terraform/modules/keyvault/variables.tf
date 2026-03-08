3cf1ebb8fef6fed1a5be91adf535ef3b260dad89
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-08 17:37:13 -0400

    Update variables.tf

--- terraform/modules/keyvault/variables.tf
+++ terraform/modules/keyvault/variables.tf
@@ -17,3 +17,13 @@ variable "object_id" {
   description = "Object ID for Key Vault access policy"
   type        = string
 }
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "object_id" {
+  description = "Azure object ID for access policy"
+  type        = string
+}
