c3f0732eb3abe72fd305278fb860acdb560688c5
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-08 17:36:25 -0400

    Add variables for keyvault module

---
+++ terraform/modules/keyvault/variables.tf
@@ -0,0 +1,19 @@
+variable "location" {
+  description = "Azure region"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Resource group for the Key Vault"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "object_id" {
+  description = "Object ID for Key Vault access policy"
+  type        = string
+}
