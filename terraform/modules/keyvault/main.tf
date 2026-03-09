2160fc1bed6c9d1309f78ebbbcca012d3924d68e
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-09 18:46:10 -0400

    Update main.tf

--- terraform/modules/keyvault/main.tf
+++ terraform/modules/keyvault/main.tf
@@ -1,11 +1,11 @@
 resource "azurerm_key_vault" "kv" {
-  name                        = "kv-landing-zone-demo"
-  location                    = var.location
-  resource_group_name         = var.resource_group_name
-  tenant_id                   = var.tenant_id
-  sku_name                    = "standard"
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+  name                       = "kvlandingzonedemo123"
+  location                   = var.location
+  resource_group_name        = var.resource_group_name
+  tenant_id                  = var.tenant_id
+  sku_name                   = "standard"
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
 
   access_policy {
     tenant_id = var.tenant_id
@@ -18,13 +18,4 @@ resource "azurerm_key_vault" "kv" {
       "List"
     ]
   }
-}
-
-module "keyvault" {
-  source = "./modules/keyvault"
-
-  location            = var.region
-  resource_group_name = azurerm_resource_group.landing_zone_rg.name
-  tenant_id           = var.tenant_id
-  object_id           = var.object_id
 }
