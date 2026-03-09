75bf835cb7a85b74c223860d4bc3249d383d7830
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-09 18:44:14 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,3 @@
 output "resource_group_name" {
   value = azurerm_resource_group.landing_zone_rg.name
 }
-
-output "log_analytics_workspace_name" {
-  value = azurerm_log_analytics_workspace.central_logs.name
-}
