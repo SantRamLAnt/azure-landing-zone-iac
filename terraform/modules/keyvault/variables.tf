22b4cb105e4255e6c804394320f2f06d2a2d8185
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-09 18:48:15 -0400

    Update variables.tf

--- terraform/modules/keyvault/variables.tf
+++ terraform/modules/keyvault/variables.tf
@@ -1,29 +1,15 @@
 variable "location" {
-  description = "Azure region"
-  type        = string
+  type = string
 }
 
 variable "resource_group_name" {
-  description = "Resource group for the Key Vault"
-  type        = string
+  type = string
 }
 
 variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
+  type = string
 }
 
 variable "object_id" {
-  description = "Object ID for Key Vault access policy"
-  type        = string
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
-variable "object_id" {
-  description = "Azure object ID for access policy"
-  type        = string
+  type = string
 }
