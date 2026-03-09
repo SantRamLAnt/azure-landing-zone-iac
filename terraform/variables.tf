fe0a9847fa0969794f51b8d14c313e9c5a9b4e58
Luis Antonio Santiago-Ramirez <lasrsecond@gmail.com>
2026-03-09 18:43:14 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,19 @@
 variable "region" {
   description = "Azure region for deployment"
   type        = string
-  default     = "East US"
 }
 
 variable "resource_group_name" {
-  description = "Name of the landing zone resource group"
+  description = "Resource group name"
   type        = string
-  default     = "rg-landing-zone-dev"
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "object_id" {
+  description = "Azure object ID for Key Vault access"
+  type        = string
 }
